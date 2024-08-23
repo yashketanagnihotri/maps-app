e1b16d8cf3ed032b627f3b954709089306346218
yashketanagnihotri <78458048+yashketanagnihotri@users.noreply.github.com>
2024-08-23 07:03:38 +0530

    Integrated ola maps and displayed the map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../OlaMapsWebSDK/style.css";
+import { OlaMaps } from "../OlaMapsWebSDK/olamaps-js-sdk.es";
 
 function App() {
+  useEffect(() => {
+    const olaMaps = new OlaMaps({
+      apiKey: "wAuLeLD9uSoxfVhKFslEJq0agit1yecn34xh7kHQ",
+    });
+
+    const myMap = olaMaps.init({
+      style:
+        "https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json",
+      container: "map",
+      center: [77.61648476788898, 12.931423492103944],
+      zoom: 15,
+    });
+  }, []);
+
   return (
     <>
       <div>
-        <h1 >Hello world</h1>
+        <h1>Maps API testing</h1>
+        <div id="map" style={{ width: "100%", height: "500px" }}></div>
       </div>
     </>
   );
